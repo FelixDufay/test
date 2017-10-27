4fd9e7f14a4af1161418652eecc17aa37996efd2
Poulpynateur <33153912+Poulpynateur@users.noreply.github.com>
2017-10-27 14:53:48 +0200

    Create rationelle.h

---
+++ rationelle.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#define ADDITION(a,b) (a+b)
+#define SOUSTRACTION(a,b) (a-b)
+#define MULTIPLICATION(a,b) ((a)*(b)
+#define DIVISION(a,b) ((a)/(b)
+
+int comparaison(int a, int b);
+int simplification(int *a, int *b);
+int pgcd (int a, int b);
